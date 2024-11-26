497dc3bb082b368e2f91039e614b10fc0fb0f426
guilhermealves54 <guilherme.alves.mo@gmail.com>
2024-11-26 18:36:16 +0000

    Changes to get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,74 @@
 #include "get_next_line.h"
 
+char	*read_buffer (fd, char *nextl);
+{
+	char	*buffer;
+	int		i;
+	size_t	b_read;
+
+	buffer = malloc ((BUFFER_SIZE + 1) * sizeof(char));
+	if (buffer == NULL)
+		return (NULL);
+	i = 0;
+	b_read = 1;
+	while (b_read > 0)
+	{
+		b_read = read (fd, buffer, BUFFER_SIZE);
+		if (b_read < 0)
+		{
+			free (buffer);
+			return (NULL);
+		}
+		buffer[b_read] = '\0';
+		nextl = ft_strjoin(nextl, buffer);
+		if (ft_strchr(nextl, '\n'))
+			break ;
+	}
+	free (buffer);
+	return (nextl);
+}
+
+char	*get_line(char *nextl)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	while (nextl[i] != '\n')
+		i++;
+	line = malloc ((i + 1) * sizeof(char));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (nextl[i] != '\n')
+	{
+		line[i] = nextl[i];
+		i++;
+	}
+	line[i] = '\0'
+	return (line);
+}
+
+/* char	*remainig_str (char *nextl)
+{
+	int		i;
+	int		n;
+
+	i = 0;
+	while (nextl[i] != '\n')
+		i++;
+	nextl[i] = '\0';
+	n = i - 1;
+	i = strlen(nextl);
+	while (n >= 0)
+	{
+		nextl[n] = nextl[i];
+		i--;
+		n--;
+	}
+	return (nextl);
+} */
+
 char	*get_next_line(int fd)
 {
 	char		*line;
@@ -10,7 +79,7 @@ char	*get_next_line(int fd)
 	nextl = read_buffer (fd, nextl);
 	if (!nextl)
 		return (NULL);
-	line = get_line(nextl);
+	line = get_line (nextl);
 	nextl = remaining_str (nextl);
 	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 # include <unistd.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1000
+#  define BUFFER_SIZE 10
 # endif
 
 char	*get_next_line(int fd);
